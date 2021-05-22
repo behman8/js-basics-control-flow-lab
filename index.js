1edc6e9ff94aca14f064bfa747478659750cbe98
Branden <brandenehman45@gmail.com>
2021-05-21 19:09:35 -0600

    commit tree 9826ccfdf93f00be604ab1b728727813fe2c76f3

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+
+  let freeSample, upCharge, noRide;
+
+  if (feet <= 400) {
+    freeSample = 'This one is on me!';
+    return freeSample;
+  } else if (feet > 2000 && feet <= 2500) {
+    upCharge = 'I will gladly take your thirty bucks.';
+    return upCharge;
+  } else if (feet > 2500) {
+    noRide = 'No can do.';
+    return noRide;
+  }
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    case 'thanks for everything':
+      return 'Bye.'
+      break;
+  }
+  return switchOnCharmFromTip()
 }
